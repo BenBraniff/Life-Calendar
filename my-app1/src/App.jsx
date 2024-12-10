85d5edf8e16884e3d112a7f46e18f235b600876a
Benjamin Braniff <Ben.Braniff37@gmail.com>
2024-12-10 16:06:34 -0500

    added grid

--- my-app1/src/App.jsx
+++ my-app1/src/App.jsx
@@ -1,30 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
 
   let myBirthday = new Date('2024-12-10');
   
+  const rows = 90;
+  const columns = 52;
+  const targetIndex = 100;
+
+  const gridStyle = {
+    justifyContent: "center",
+    display: "grid",
+    gridTemplateColumns: `repeat(${columns}, 10px)`,
+    gap: "2px",
+    margin: "30px"
+  };
+
+  const squares = Array.from({ length: rows * columns }, (_, index) => (
+    <div
+      key={index}
+      style={{
+        width: "10px",
+        height: "10px",
+        backgroundColor: index <= targetIndex ? "black" : "lightgray",
+      }}
+    ></div>
+  ));
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>your mom</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <h1>Life Calander</h1>
+      <h2>by: Ben Braniff</h2>
+      <div style={gridStyle}>{squares}</div>
+    </body>
   );
-}
+};
 
 export default App;
